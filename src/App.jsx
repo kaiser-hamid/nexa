f2ede3fe6a7aa7c8f97e298741fdfb252a672f3f
Kaiser Hamid <khtusher.mail@gmail.com>
2026-07-13 21:55:18 +0600

    deploy and check in real

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
       <section id="center">
         <h2>Vite + React</h2>
         <p>This is a simple reactjs application. That is being used for CI/CD purposes.</p>
-        <p>Line added from development branch updated.</p>
         <p>Hello CI/CD pipeline. I have been pushed from github action</p>
       </section>
     </>
